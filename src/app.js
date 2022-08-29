14e25f304ed239d69f7e131b00435a29ce9bc170
JennyGlover <81665624+JennyGlover@users.noreply.github.com>
2022-08-28 21:02:17 -0400

    edited page styles and added heading and  footer

--- src/app.js
+++ src/app.js
@@ -2,15 +2,11 @@
 
 
 
-
-let h1 = document.createElement('h1');
-h1.innerHTML = 'Hi there';
-
 const myDiv = document.createElement('div')
 
 
+
 const pokemonsLink = document.createElement('a');
-console.log(pokemonsLink);
 //pokemonsLink.setAttribute('href', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/');
 
 
@@ -25,10 +21,28 @@ let myLink =`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/po
 const myImage = document.createElement('img');
 myImage.setAttribute('src', myLink);
 myImage.classList.add('pokemon-image');
- console.log('hello');
- document.body.append(myImage.cloneNode(true));
+ myDiv.classList.add('myDiv');  
+ myDiv.innerText = "";
+ myDiv.appendChild(myImage.cloneNode(true)); 
+ const p = document.createElement('p');
+p.innerHTML = `#${total}`
+myDiv.appendChild(p.cloneNode(true));
+document.body.append(myDiv.cloneNode(true));
 
 }
+
+let num = Math.floor(Math.random() * 151);
+
+let pokemonLink = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${num}.png`;
+randomPokemon = document.querySelector('.random-pokemon');
+
+randomPokemon.setAttribute('src', pokemonLink)
+
+const myFooter = document.createElement('div');
+myFooter.setAttribute('class', 'my-footer' );
+myFooter.innerHTML = 'Open Source Code by Jenny Glover Saunders';
+console.log("Is this working")
+document.body.append(myFooter.cloneNode(true));
 
 // for (i = 0; i<100; i++){
 //     div.appendChild(allButtons.cloneNode(true))
